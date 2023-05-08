ed5db45a1fac902e33a4ef8a8b3a6f293e9cd79c
Han <hieuhantr@gmail.com>
2023-05-08 16:45:07 -0700

    Update logic and style

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 let currentResult;
-let displayValue = '';
-let previousValue = '';
 let operator;
 let operatorForMath;
 let num1;
@@ -16,101 +14,148 @@ const numberButtons = Array.from(numberButtonsNode);
 const operatorButtonsNode = document.querySelectorAll('.operators');
 const operatorButtons = Array.from(operatorButtonsNode);
 
+
+
 //add event: display number when clicked
 numberButtons.forEach((numberButton) => {
     numberButton.addEventListener('click', () => {
         //reset bigDisplay
-        if (bigDisplay.innerText == '0') {
-            displayValue = '';
+        if ((bigDisplay.innerText == '0' && numberButton.innerText !== '.') || bigDisplay.innerText == num1) {
+            bigDisplay.innerText = '';
         }
-        if (!operator) { //for first number calculation
-            displayValue += numberButton.innerText;
-        }
-        else { //from 0.5 calculation
-            displayValue = numberButton.innerText;
-            // operatorButtons.forEach((operatorButton) => {
-            //     operatorButton.style.backgroundColor = '#282A31';
-            // })
-            if (currentResult && num1) {
-                previousValue = currentResult + ' ' + operator;
+        if (bigDisplay.innerText.lastIndexOf('.') < 0 || numberButton.innerText !== '.') {
+            if (bigDisplay.innerText.length < 10 || operator) {
+                if (bigDisplay.innerText.length < 10) {
+                    bigDisplay.innerText += numberButton.innerText;
+                }
             }
         }
-        bigDisplay.innerText = displayValue;
     })
-})
+});
 
 //add event: bigDisplay operator when clicked, store operator in operatorForMath
 operatorButtons.forEach((operatorButton) => {
     operatorButton.addEventListener('click', () => {
-        if (!currentResult && !num1 && !num2) { //first calculation
-            num1 = Number(bigDisplay.innerHTML);
-            console.log(num1, operator, num2, currentResult)
+        if (operatorButton.innerText == '%') {
+            operator = operatorButton.innerText;
+            calcResult();
         }
-        else {
-            if (!currentResult && num1 && !num2)//if did not click equal
-            {
-                console.log(num1, operator, num2, currentResult)
-                getNum2;
-                calcResult;
-                console.log(num1, operator, num2, currentResult)
-             }
-            num1 = currentResult; //from 2nd calculation
+        else { // for the rest: + - * /
+            if (!num1) { //first calculation
+                num1 = Number(bigDisplay.innerHTML);
+            }
+            else {
+                //no equal, straight to operator
+                if (operator && !currentResult) {
+                    getNum2();
+                    calcResult();
+                }
+            }
+            //save operator
+            operatorButton.style.backgroundColor = 'lightBlue';
+            operatorButton.addEventListener('mouseleave', () => {
+                operatorButton.style.backgroundColor = 'lightBlue';
+            });
+            operator = operatorButton.innerText;
         }
-        //set operator
-        operator = operatorButton.innerText;
-        // operatorButton.style.backgroundColor = 'lightBlue';
     })
 })
 
-
 //get num2
 const getNum2 = () => {
-    num2 = Number(displayValue)
+    if (operator) {
+        num2 = Number(bigDisplay.innerText);
+    }
 }
 
 //calculate result
 const calcResult = () => {
-    switch (operator) {
-        case '+':
-            currentResult = num1 + num2;
-            break;
-        case '−':
-            currentResult = num1 - num2;
-            break;
-        case '×':
-            currentResult = num1 * num2;
-            break;
-        case '÷':
-            currentResult = num1 / num2;
-            break;
+    operatorButtons.forEach(operatorButton => {
+        //style buttons
+        operatorButton.style.backgroundColor = '#282A31';
+        if (operatorButton.innerText !== '%') {
+            operatorButton.addEventListener('mouseleave', () => {
+                operatorButton.style.backgroundColor = '#282A31';
+            });
+            operatorButton.addEventListener('mousemove', () => {
+                operatorButton.style.backgroundColor = 'rgba(255,255,255,0.2)';
+            });
+            operatorButton.addEventListener('click', () => {
+                operatorButton.style.backgroundColor = '#lightBlue';
+                operatorButton.addEventListener('mouseleave', () => {
+                    operatorButton.style.backgroundColor = 'lightBlue';
+                });
+            });
+        }
+    });
+    //error case from previously
+    if (num1 == 'Error') {
+        currentResult = 'Error';
+    } else if (!operator) {
+        currentResult = bigDisplay.innerText;
+    }
+    //calculate
+    else {
+        switch (operator) {
+            case '+':
+                currentResult = Number(num1 + num2);
+                break;
+            case '−':
+                currentResult = Number(num1 - num2);
+                break;
+            case '×':
+                currentResult = Number(num1 * num2)
+                break;
+            case '÷':
+                if (num2 == '0') {
+                    currentResult = 'Error';
+                    break;
+                }
+                currentResult = num1 / num2;
+                break;
+            case '%':
+                currentResult = Number(bigDisplay.innerText) / 100;
+                break;
+        }
+    }
+    //in case of long result
+    if (String(currentResult).length > 11) {
+        if (String(currentResult).indexOf('.') >= 0) {
+            currentResult = String(currentResult).slice(0, 10);
+        } else {
+            currentResult = String(currentResult).slice(0, 10) + '...';
+        }
     }
     bigDisplay.innerText = currentResult;
-    num1 = '';
+    num1 = currentResult;
+    currentResult = '';
     num2 = '';
     operator = '';
-    displayValue = '0';
 }
 
-const equal = document.querySelector('#equal')
+//get result
+const equal = document.querySelector('#equal');
 equal.addEventListener('click', () => {
-    getNum2;
-    calcResult;
+    getNum2();
+    calcResult();
 })
 
+//clear all
 const clear = document.querySelector('#AC')
 clear.addEventListener('click', () => {
     num1 = '';
-    num2 = '';
+    num2 = 0;
     operatorForMath = null;
     operator = null;
     currentResult = '';
-    displayValue = '0';
-    bigDisplay.innerText = displayValue;
+    bigDisplay.innerText = '0';
     operatorButtons.forEach(operatorButton => {
         operatorButton.style.backgroundColor = '#282A31';
     })
 })
 
-//operate %
-//divide 0
-//decimal cut
+//erase one number
+const erase = document.querySelector('#erase');
+erase.addEventListener('click', () => {
+    bigDisplay.innerText = bigDisplay.innerText.slice(0, bigDisplay.innerText.length - 1);
+});
